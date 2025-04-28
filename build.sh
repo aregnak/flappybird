d960f5cab975ca56187c5145bae95cf2ab3325c9
areg <areg.nakashian@gmail.com>
2025-04-28 14:39:14 -0400

    removed C++20 standard

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Building Project..."
 
-if (g++ src/*.cpp -o flappy -std=c++20 -I ./include -lsfml-system -lsfml-window -lsfml-graphics -lsfml-audio); then
+if (g++ src/*.cpp -o flappy -I ./include -lsfml-system -lsfml-window -lsfml-graphics -lsfml-audio); then
   echo "Compiled successfully. Use ./flappy to run."
 else
   echo "Error occured while compiling. Exiting."
