598e8ec283625d65507c1b0a7ff13fb58047e059
areg <areg.nakashian@gmail.com>
2025-05-30 23:45:11 -0400

    Main.cpp cleanup
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,5 @@
 #include "game.h"
 
-#define INITIAL_WIDTH 800
-#define INITIAL_HEIGHT 800
-#define ASPECT_RATIO (float)INITIAL_WIDTH / INITIAL_HEIGHT
-
-#define WALL_GAP 400
-
 int main()
 {
     Game game;
